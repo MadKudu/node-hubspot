aa23da3f44250fc26dbb099af064f6eea6a8b186
Filipe Ferreira <filipe.ferreira@sclgroup.cc>
2016-09-12 18:13:53 +0100

    More indent test into function, added missing test for engagements and companies

---
+++ test/engagements.js
@@ -0,0 +1,25 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var Client = require('../index.js');
+var client = new Client();
+var api_key = 'demo';
+client.useKey(api_key);
+
+
+describe('Engagements', function () {
+
+  describe('Create an Engagement', function () {
+    it('Should create an engagement', function (done) {
+      client.engagements.create({"engagement": {"active": true, "ownerId": 1, "type": "NOTE", "timestamp": 1409172644778}, "associations": {"contactIds": [2], "companyIds": [ ], "dealIds": [ ], "ownerIds": [ ]}, "metadata": {"body": "note body"}},
+        function (err, data, res) {
+          expect(err).to.equal(null);
+          expect(res.statusCode).to.equal(400);
+          expect(data.status).to.equal('error');
+          expect(data.message).to.equal('one or more associations were invalid');
+          done();
+        }
+      )
+    })
+  });
+});
