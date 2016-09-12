b02e6f53dd7ed8031492bdf637c648ae42993ed0
Filipe Ferreira <filipe.ferreira@sclgroup.cc>
2016-09-12 16:08:26 +0100

    Refined lists test to have type based checking to data.

--- test/lists.js
+++ test/lists.js
@@ -9,65 +9,66 @@ client.useKey(api_key);
 
 describe('Lists', function () {
 
-    describe('Get List', function(){
-        it('Should return contact list', function (done) {
-            client.lists.get(function(err, data, res) {
-                if (err) { throw err; }
-                expect(res.statusCode).to.equal(200);
-                expect(data).to.be.defined;
-                expect(data.lists).to.be.defined;
-                done();
-            })
-        });
+  describe('Get List', function(){
+    it('Should return contact list', function (done) {
+      client.lists.get(function(err, data, res) {
+        if (err) { throw err; }
+        expect(res.statusCode).to.equal(200);
+        expect(data).to.be.a('object');
+        expect(data.lists).to.be.a('array');
+        done();
+      })
     });
+  });
 
-    describe('Get List by Id', function(){
-        it('Should return one contact in a list', function (done) {
-            client.lists.getOne(1,function(err,data, res) {
-                if (err) { throw err; }
-                expect(res.statusCode).to.equal(200);
-                expect(data).to.be.defined;
-                expect(data.name).to.be.defined;
-                done();
-            })
-        });
+  describe('Get List by Id', function(){
+    it('Should return one contact in a list', function (done) {
+      client.lists.getOne(1,function(err,data, res) {
+        if (err) { throw err; }
+        expect(res.statusCode).to.equal(200);
+        expect(data).to.be.a('object');
+        expect(data.name).to.equal('update list name');
+        done();
+      })
     });
+  });
 
-    describe('Get Contacts In A List', function(){
-        it('Should return all contacts in a list', function (done) {
-            client.lists.getContacts(1,function(err, data, res) {
-                if (err) { throw err; }
-                expect(res.statusCode).to.equal(200);
-                expect(data).to.be.defined;
-                done();
-            })
-        });
+  describe('Get Contacts In A List', function(){
+    it('Should return all contacts in a list', function (done) {
+      client.lists.getContacts(1,function(err, data, res) {
+        if (err) { throw err; }
+        expect(res.statusCode).to.equal(200);
+        expect(data).to.be.a('object');
+        expect(data.contacts).to.be.a('array');
+        done();
+      })
     });
+  });
 
-    describe('Get recently updated and created contacts', function(){
-        it('Should a list of recently_updated contacts', function (done) {
-            client.lists.getRecentContacts(1,function(err, data, res) {
-                if (err) { throw err; }
-                expect(res.statusCode).to.equal(200);
-                expect(data).to.be.defined;
-                expect(data.contacts).to.be.defined;
-                done();
-            })
-        });
+  describe('Get recently updated and created contacts', function(){
+    it('Should a list of recently_updated contacts', function (done) {
+      client.lists.getRecentContacts(1,function(err, data, res) {
+        if (err) { throw err; }
+        expect(res.statusCode).to.equal(200);
+        expect(data).to.be.a('object');
+        expect(data.contacts).to.be.a('array');
+        done();
+      })
     });
+  });
 
-    describe('Add existing contacts to a list', function(){
-        it('Should add contact to a list of contacts', function (done) {
-            client.lists.addContacts(3,{
-              "vids": [61571], "emails": ["testingapis@hubspot.com"]
-        },function(err, data, res) {
-                if (err) { throw err; }
-                expect(res.statusCode).to.equal(200);
-                expect(data).to.be.defined;
-                expect(data.updated).to.be.defined;
-                done();
-            })
-        });
+  describe('Add existing contacts to a list', function(){
+    it('Should add contact to a list of contacts', function (done) {
+      client.lists.addContacts(3,{
+        "vids": [61571], "emails": ["testingapis@hubspot.com"]
+      },function(err, data, res) {
+        if (err) { throw err; }
+        expect(res.statusCode).to.equal(200);
+        expect(data).to.be.a('object');
+        expect(data.updated).to.be.a('array');
+        done();
+      })
     });
+  });
 
 });
