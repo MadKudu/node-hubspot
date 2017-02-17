9c4dd3f934293efb03a77452649619741cdba0e2
Brian Falk <falk@logicparty.org>
2017-02-17 10:58:41 -0500

    add test

--- test/companies.js
+++ test/companies.js
@@ -8,6 +8,17 @@ var api_key = '5e4e9b8c-9146-4d90-95eb-8fe94edd3175';
 
     describe('Companies', function () {
 
+      describe('Get Company by Id', function() {
+        it('Should return a company', function (done) {
+          client.companies.getById(322620707, function(err, data, res) {
+            if (err) { throw err; }
+            expect(res.statusCode).to.equal(200);
+            expect(data).to.be.a('object');
+            done();
+          });
+        });
+      });
+
       describe('Get Recently Created', function(){
         it('Should return recently created companies', function (done) {
           client.companies.getRecentlyCreated(function(err, data, res) {
