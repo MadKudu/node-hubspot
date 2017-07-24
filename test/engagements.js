56f6831cd03d35ed92250c8797902ad5f0318151
Paul Cothenet <paul@madkudu.com>
2017-07-24 14:39:16 -0700

    Add engagements.get

--- test/engagements.js
+++ test/engagements.js
@@ -1,17 +1,27 @@
-// const chai = require('chai')
-// const expect = chai.expect
-//
-// const Hubspot = require('..')
-// const hubspot = new Hubspot({ apiKey: 'demo' })
-//
-// describe('Engagements', function () {
-//   describe('create', function () {
-//     it('Should create an engagement', function () {
-//       const payload = { 'engagement': { 'active': true, 'ownerId': 1, 'type': 'NOTE', 'timestamp': 1409172644778}, 'associations': {'contactIds': [2], 'companyIds': [ ], 'dealIds': [ ], 'ownerIds': [ ]}, 'metadata': {'body': 'note body' } }
-//       return hubspot.engagements.create(payload).then(data => {
-//         expect(data.status).to.equal('error')
-//         expect(data.message).to.equal('one or more associations were invalid')
-//       })
-//     })
-//   })
-// })
+const chai = require('chai')
+const expect = chai.expect
+
+const Hubspot = require('..')
+const hubspot = new Hubspot({ apiKey: 'demo' })
+
+describe('Engagements', function () {
+  describe('Get All Engagements', function () {
+    it('Should return engagement properties', function () {
+      return hubspot.engagements.get().then(data => {
+        expect(data).to.be.an('object')
+        expect(data.results).to.be.a('array')
+        expect(data.results[0]).to.have.a.property('engagement')
+      })
+    })
+  })
+
+  // describe('create', function () {
+  //   it('Should create an engagement', function () {
+  //     const payload = { 'engagement': { 'active': true, 'ownerId': 1, 'type': 'NOTE', 'timestamp': 1409172644778 }, 'associations': { 'contactIds': [2], 'companyIds': [ ], 'dealIds': [ ], 'ownerIds': [ ] }, 'metadata': { 'body': 'note body' } }
+  //     return hubspot.engagements.create(payload).then(data => {
+  //       expect(data.status).to.equal('error')
+  //       expect(data.message).to.equal('one or more associations were invalid')
+  //     })
+  //   })
+  // })
+})
