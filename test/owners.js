0f0e8c31c6c3a095d01cc80a861fb259fb9da887
Edward Loveall <edward@thoughtbot.com>
2018-12-05 11:39:07 -0500

    Proof of concept for extracting Nock
    Why:
    
    Every request also makes an additional request to the rate limit api.
    Our new mockEndpoint function mocks the call to the rate limit api and
    also mocks whatever call for the current test.
    
    PR:
    
    Introduces one test using nock and the nockHelper
    
    Co-Authored-By: Matt Sumner <matt.m.sumner@gmail.com>

--- test/owners.js
+++ test/owners.js
@@ -1,10 +1,13 @@
 const chai = require('chai')
 const expect = chai.expect
+const nockHelper = require('./helpers/nock_helper')
 
 const Hubspot = require('..')
 const hubspot = new Hubspot({ apiKey: 'demo' })
 
 describe('Owners', function() {
+  before(nockHelper.mockEndpoint('/owners/v2/owners', []))
+  after(nockHelper.resetNock)
   describe('get', function() {
     it('Should return all owners', function() {
       return hubspot.owners.get().then(data => {
