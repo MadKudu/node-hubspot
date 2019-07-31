d118931f896b093c1de1865394a71b804bf62434
antoine-pous <antoine.pous@dvp.io>
2019-07-31 11:35:55 +0200

    Implement owners getById, types, and test from HS demo API for consistency"

--- test/owners.js
+++ test/owners.js
@@ -1,17 +1,46 @@
 const { expect } = require('chai')
-const fakeHubspotApi = require('./helpers/fake_hubspot_api')
 const Hubspot = require('..')
 
 describe('Owners', function() {
-  const ownersGetEndpoint = { path: '/owners/v2/owners', response: [] }
-  fakeHubspotApi.setupServer({ demo: true, getEndpoints: [ownersGetEndpoint] })
-
   describe('get', function() {
     it('Should return all owners', function() {
       const hubspot = new Hubspot({ apiKey: 'demo' })
 
       return hubspot.owners.get().then(data => {
         expect(data).to.be.a('array')
+      })
+    })
+  })
+
+  describe('getById', function() {
+    it('should return one owner', function() {
+      const hubspot = new Hubspot({ apiKey: 'demo' })
+
+      return hubspot.owners.getById(66).then(data => {
+        expect(data).to.be.a('object')
+        expect(data).to.have.all.keys(
+          'portalId',
+          'ownerId',
+          'type',
+          'firstName',
+          'lastName',
+          'email',
+          'createdAt',
+          'updatedAt',
+          'remoteList',
+          'hasContactsAccess',
+          'activeUserId',
+          'userIdIncludingInactive',
+          'isActive'
+        )
+        expect(data.remoteList[0]).to.have.all.keys(
+          'id',
+          'portalId',
+          'ownerId',
+          'remoteId',
+          'remoteType',
+          'active'
+        )
       })
     })
   })
