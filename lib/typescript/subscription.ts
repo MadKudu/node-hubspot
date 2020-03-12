d151ad3ba3a2b71e10e6a04a31c8506ce8377ff8
Tigran Muradyan <mrdntgrn@gmail.com>
2020-03-12 18:48:36 +0400

    Fix subscription typescript definition to have unsubscribe function

--- lib/typescript/subscription.ts
+++ lib/typescript/subscription.ts
@@ -3,6 +3,7 @@ import { RequestPromise } from 'request-promise'
 declare class Subscription {
   get(opts?: {}): RequestPromise
   subscribeToAll(email: string): RequestPromise
+  unsubscribe(email: string): RequestPromise
 }
 
 export { Subscription }
